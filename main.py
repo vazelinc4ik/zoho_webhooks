574ecd9b1d7fbfe23360f4f7ab9f8b81a52764ad
vazelinc4ik <issachannel@gmail.com>
2025-05-14 20:28:31 +0300

    added sales uri

--- main.py
+++ main.py
@@ -10,7 +10,15 @@ app = FastAPI()
 @app.post("/zoho-webhooks/inventory-adjustment")
 async def adjust_eckwid_inventory(
     request: Request
-) -> None:
+) -> dict:
     data = await request.json()
     print(data)
+    return {"status": "ok"}
 
+@app.post("/zoho-webhooks/sales")
+async def adjust_eckwid_inventory(
+    request: Request
+) -> dict:
+    data = await request.json()
+    print(data)
+    return {"status": "ok"}
