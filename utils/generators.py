f0207a7b465feb41bfeba7dc43672c60476aa425
vazelinc4ik <issachannel@gmail.com>
2025-05-30 12:02:46 +0300

    Отладка в генераторе

--- utils/generators.py
+++ utils/generators.py
@@ -10,9 +10,13 @@ from crud import StoresCRUD
 
 async def get_ecwid_api(request: Request) -> EcwidApi:
     zoho_organization_id = request.headers.get("x-com-zoho-organizationid")
+    print(zoho_organization_id)
     if not zoho_organization_id:
         raise HTTPException(status_code=400, detail="Missing organization ID header")
-    store = await StoresCRUD.find_one_or_none(zoho_organization_id=zoho_organization_id)
+    try:
+        store = await StoresCRUD.find_one_or_none(zoho_organization_id=zoho_organization_id)
+    except Exception as e:
+        print(f'Failed to find store {e}')
     if not store:
         raise HTTPException(status_code=404, detail="Store not found")
     return EcwidApi(store.ecwid_store_id, settings.ecwid_settings.ecwid_app_secret)
