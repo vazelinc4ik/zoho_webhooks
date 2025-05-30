e7bb12b193cca11369920417e4985b5c96fd3cd6
vazelinc4ik <issachannel@gmail.com>
2025-05-30 12:09:28 +0300

    Еше фиксы

--- utils/generators.py
+++ utils/generators.py
@@ -1,16 +1,22 @@
 from typing import AsyncGenerator, Generator
 
 from ecwid_api import EcwidApi
-from fastapi import HTTPException, Request
+from fastapi import Depends, HTTPException, Request
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.config import settings
 from core import async_session_maker
 from crud import StoresCRUD
 
-async def get_ecwid_api(request: Request) -> EcwidApi:
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+async def get_ecwid_api(
+    request: Request,
+    db: AsyncSession = Depends(get_db)
+) -> EcwidApi:
     zoho_organization_id = request.headers.get("x-com-zoho-organizationid")
-    print(zoho_organization_id)
     if not zoho_organization_id:
         raise HTTPException(status_code=400, detail="Missing organization ID header")
     try:
@@ -20,8 +26,3 @@ async def get_ecwid_api(request: Request) -> EcwidApi:
     if not store:
         raise HTTPException(status_code=404, detail="Store not found")
     return EcwidApi(store.ecwid_store_id, settings.ecwid_settings.ecwid_app_secret)
-
-
-async def get_db() -> AsyncGenerator[AsyncSession, None]:
-    async with async_session_maker() as session:
-        yield session
