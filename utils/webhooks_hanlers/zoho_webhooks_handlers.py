7498826c6f1f52a2fc1e582bca174d90b6fe95ac
vazelinc4ik <issachannel@gmail.com>
2025-07-23 01:14:27 +0300

    add(logging)

--- utils/webhooks_hanlers/zoho_webhooks_handlers.py
+++ utils/webhooks_hanlers/zoho_webhooks_handlers.py
@@ -115,6 +115,7 @@ class BaseHandler(ABC):
 
         store = await cls._find_store_entity_in_database(db, zoho_organization_id=zoho_organization_id)
         items_data = await cls._get_items_data_from_request(request)
+        logger.info(json.dumps(items_data, indent=4))
         for item in items_data:
             warehouse_id = item.get('warehouse_id', None)
             if warehouse_id and warehouse_id != TARGET_WH_ID:
